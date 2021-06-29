1b794ac43f48471db4b692db3452cb9262a26209
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2021-06-29 12:42:29 -0400

    add Haskell support

---
+++ modes/haskell.el
@@ -0,0 +1,3 @@
+(use-package lsp-mode)
+(use-package lsp-ui)
+(use-package lsp-haskell)
--- modes/modes.el
+++ modes/modes.el
@@ -1,3 +1,4 @@
 ;; (load "~/.emacs.d/modes/mercury.el")
 (load "~/.emacs.d/modes/prolog.el")
+(load "~/.emacs.d/modes/haskell.el")
 
