e31766a4a977b7c432d95d8a43377c9fde90cf49
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2021-07-01 15:50:08 -0400

    Fix package download issues on older Emacs versions.

--- geoff-init.el
+++ geoff-init.el
@@ -1,3 +1,7 @@
+;; Fixes "Bad request" package download issues.
+(if (and (version< emacs-version "26.3") (>= libgnutls-version 30604))
+    (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+
 (load "~/.emacs.d/geoff-init-packages.el")
 (load "~/.emacs.d/geoff-init-appearance.el")
 (load "~/.emacs.d/geoff-init-misc.el")
